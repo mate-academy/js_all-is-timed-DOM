f07aac7e478d92cbfee474c7cbaef4a398033fc9
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-20 20:21:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+setInterval(() => {
+  let spendSeconds = localStorage.getItem('spendSeconds');
+
+  if (!spendSeconds) {
+    localStorage.setItem('spendSeconds', 0);
+  } else {
+    localStorage.setItem('spendSeconds', ++spendSeconds);
+  }
+
+  console.log(`You spend on this page ${localStorage.getItem('spendSeconds')} seconds.`);
+}, 1000);
