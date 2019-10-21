b69ed32bdb13bf1ccf2235074cb3e619202eef9b
Serhii <gespenst.serge@yandex.ru>
2019-10-21 15:53:39 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+setInterval(() => {
+  localStorage.timer++;
+  document.getElementById('timer')
+    .textContent = `You spent ${localStorage.timer}
+    seconds on this website`;
+}, 1000);
