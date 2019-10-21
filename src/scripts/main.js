e0402191995c2bc4a14c0806d7a26e870df2406d
Vitalii Starodub <Vitalii.Starodub@playtech.com>
2019-10-21 15:48:46 +0300

    'solution_vitalii'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let seconds = localStorage.getItem('seconds');
+const element = document.body.querySelector('#timer');
+
+function incrementSeconds() {
+  seconds++;
+  element.innerHTML = `You have been there for ${seconds} seconds`;
+  localStorage.setItem('seconds', seconds);
+}
+
+const startTimer = () => {
+  setInterval(incrementSeconds, 1000);
+};
+
+startTimer();
