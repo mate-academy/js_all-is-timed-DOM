cd2d77c1bbded7ee4b18e43600e3898cbd30e202
Sijey <Popov040391@gmail.com>
2019-10-21 17:25:11 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,7 @@ document.body.innerText = `You are ${timer} seconds of website`;
 setInterval(() => {
   timer++;
   document.body
-    .innerText = `You are ${localStorage.getItem('timer')} seconds of website`;
+    .innerText = `You are ${localStorage
+      .getItem('timer') || 0} seconds of website`;
   localStorage.setItem('timer', timer);
 }, 1000);
