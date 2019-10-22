fe53cc806a51afc9e9549f53246100cc7263401e
Sijey <Popov040391@gmail.com>
2019-10-22 22:14:03 +0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 let timer = localStorage.getItem('timer') || 0;
-const secondsSpan = document.querySelector('#seconds');
+const timerSpan = document.querySelector('#seconds');
 
-secondsSpan.innerText = timer;
+timerSpan.innerText = timer;
 setInterval(() => {
   timer++;
-  secondsSpan.innerText = timer;
+  timerSpan.innerText = timer;
   localStorage.setItem('timer', timer);
 }, 1000);
