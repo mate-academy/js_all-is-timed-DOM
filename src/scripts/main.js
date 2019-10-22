72ef71206e235670c7423f89aff723759772b24e
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-10-22 20:26:33 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+const timer = document.getElementById('timer');
+let userTimer = localStorage.getItem('userTimer') || 0;
+timer.textContent = userTimer;
+
+setInterval(() => {
+  localStorage.setItem('userTimer', ++userTimer);
+  timer.textContent = userTimer;
+}, 1000);
