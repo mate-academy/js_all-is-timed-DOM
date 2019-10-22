ebe5d9dcc886560047380e50a7f80ee0c27972ad
VadimK <djvadya16@gmail.com>
2019-10-22 11:52:29 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+let timer = document.getElementById('timer');
+
+if (!localStorage.hasOwnProperty('timeInPage')) {
+  localStorage.setItem('timeInPage', 0);
+}
+
+timer.innerText = `${localStorage.getItem('timeInPage')}s`;
+
+let timerId = setInterval(() => {
+  localStorage['timeInPage']++;
+  timer.innerText = `${localStorage.getItem('timeInPage')}s`;
+}, 1000);
+
+
