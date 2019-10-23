ad0df9974f1bece6b9ad12cdda7e892316090f22
Anna Kazantseva <annetastara@gmail.com>
2019-10-23 11:57:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+const timer = document.querySelector('#timer');
+let timeCounter = localStorage.getItem('localTime') || 0;
+timer.textContent = timeCounter;
+
+setInterval(() => {
+  timeCounter++;
+  timer.textContent = timeCounter;
+  localStorage.setItem('localTime', timeCounter);
+}, 1000);
