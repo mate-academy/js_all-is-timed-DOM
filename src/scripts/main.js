4bc2702e35da3079469f2cc9dd37e9a04397af7f
YuraBobeshko <gorobech55@gmail.com>
2019-10-23 21:05:30 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let counter = localStorage.getItem('spentTime') || 0;
+const time = document.getElementById('timer');
+time.textContent = counter;
+
+setInterval(() => {
+  counter++;
+  localStorage.setItem('spentTime', counter);
+  time.textContent = counter;
+}, 1000);
