20fecc06eb67af2067f909cee4fdda18c250e618
nikakarelets <v.karelets@gmail.com>
2019-10-23 13:17:09 +0300

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
-let secondsCounter = localStorage.getItem('seconds') || -1;
+let secondsCounter = localStorage.getItem('seconds') || 0;
 const seconds = document.getElementById('seconds');
+seconds.innerHTML = secondsCounter;
 
 function clock() {
   secondsCounter++;
