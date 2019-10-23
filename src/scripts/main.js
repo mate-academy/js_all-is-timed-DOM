8789cee57838dec4a44e3858ee028bb54daeaff3
Andrii <farikpite@gmail.com>
2019-10-23 11:51:35 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,11 @@
 'use strict';
+const time = 'timer';
 const item = document.querySelector('#timer');
-let timer = localStorage.getItem('timer');
+let timer = localStorage.getItem(time) || 0;
 item.textContent = `You are here ${timer} seconds`;
 
 setInterval(() => {
   timer++;
-  localStorage.setItem('timer', timer);
+  localStorage.setItem(time, timer);
   item.innerHTML = `You are here ${timer} seconds`;
 }, 1000);
