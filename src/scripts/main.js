6e38dcaba05de6103da7640675babb5ae0b2202a
Leon <FeelineKing@gmail.com>
2019-10-24 07:21:00 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let timer = document.querySelector('#timer');
+let count = localStorage.getItem('count') || 0;
+
+timer.innerText = `I stay ${count} seconds in this page`;
+
+setInterval(function() {
+  count++;
+  localStorage.setItem(`count`, count);
+  timer.innerText = `I stay ${count} seconds in this page`;
+}, 1000);
