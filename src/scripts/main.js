0e025fd5e85ffa6952ed7f4ba6e8e4923606a119
Shurkodr <sashaglazunov2@gmail.com>
2019-10-24 13:22:41 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+const timerElement = document.querySelector('#timer');
+let timer = localStorage.getItem('currentTime') || 0;
+
+showTime();
+
+function showTime() {
+  timerElement.innerText = `Time you have spent ${timer}`;
+}
+
+function fun() {
+  timer++;
+  localStorage.setItem('currentTime', timer);
+  showTime();
+}
+
+setInterval(fun, 1000);
