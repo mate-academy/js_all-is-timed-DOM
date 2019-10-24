efde3aefafcaebeb8c9d8656babf988bd8a405ac
Viktoriia <jhunt@ukr.net>
2019-10-24 09:29:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let secondsTimer = localStorage.getItem('counter') || 0;
+const counter = document.querySelector('#counter');
+counter.innerHTML = secondsTimer;
+
+setInterval(() => {
+  secondsTimer++;
+  localStorage.setItem('counter', secondsTimer);
+  counter.innerHTML = secondsTimer;
+}, 1000);
